be88e96ed137e9f72d6f36719d5ebaa22e696a8b
Kauã Nunes <kauanunnes@hotmail.com>
2021-08-17 09:36:11 -0300

    menu hamburguer scroll consertado

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 let menuActivated = false;
 let menuHamburguer = document.querySelector("div.menu-hamburguer");
 let activeClass = document.createAttribute("class");
+const body = document.querySelector("body");
 
 function showMenuHamburguer() {
   activeClass.value = "menu-hamburguer active";
   menuHamburguer.setAttributeNode(activeClass);
   console.log(menuActivated);
+  body.style.overflowY = "hidden";
 }
 
 function hiddenMenuHamburguer() {
@@ -13,4 +15,5 @@ function hiddenMenuHamburguer() {
   menuHamburguer.removeAttributeNode(hamburguerClass);
   activeClass.value = "menu-hamburguer";
   menuHamburguer.setAttributeNode(activeClass);
+  body.style.overflowY = "initial";
 }
